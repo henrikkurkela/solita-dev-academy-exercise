ef8ae1b3e86494c1405810bf41e2c2bc5d756ceb
Henrik Kurkela <henrik.kurkela@gmail.com>
2021-12-17 21:02:02 +0200

    Better success message handling

--- resources/views/components/success-message.blade.php
+++ resources/views/components/success-message.blade.php
@@ -1,21 +1,10 @@
-@if (isset($success))
+@if (isset($success) || session('success'))
 <div {{ $attributes }}>
     <div class="font-medium text-green-600">
         {{ __('Success!') }}
     </div>
-
     <ul class="mt-3 list-disc list-inside text-sm text-green-600">
-        {{ $success }}
-    </ul>
-</div>
-@elseif(session('success'))
-<div {{ $attributes }}>
-    <div class="font-medium text-green-600">
-        {{ __('Success!') }}
-    </div>
-
-    <ul class="mt-3 list-disc list-inside text-sm text-green-600">
-        {{ session('success') }}
+        {{ $success ?? session('success') }}
     </ul>
 </div>
 @endif
